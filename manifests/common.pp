7cdf522306d8ea6c8cd74443107e67ffb83b2f1d
root <root@vmdm0006.cern.ch>
2011-05-13 12:11:42 +0200

    Added EMI repositories, moved glite ones to separate file.

--- manifests/common.pp
+++ manifests/common.pp
@@ -16,12 +16,6 @@ class dms::common {
   package { "lcgdm-libs": require => Package["yum-protectbase"] }
 
   yumrepo {
-    "glite-global-etics":
-      descr    => "ETICS Global gLite Registered Repository",
-      baseurl  => "http://etics-repository.cern.ch/repository/pm/registered/repomd/global/org.glite/sl5_x86_64_gcc412",
-      gpgcheck => 0,
-      enabled  => 1,
-      protect  => 0;
     "nfsv41":
       descr    => "Scientific Linux pNFS enabled kernel",
       baseurl  => "https://grid-deployment.web.cern.ch/grid-deployment/dms/yum/sl5/nfs41",
--- manifests/head.pp
+++ manifests/head.pp
@@ -27,6 +27,12 @@ class dms::head {
       gpgcheck => 0,
       enabled  => 1,
       protect  => 1;
+    "glite-global-etics":
+      descr    => "ETICS Global gLite Registered Repository",
+      baseurl  => "http://etics-repository.cern.ch/repository/pm/registered/repomd/global/org.glite/sl5_x86_64_gcc412",
+      gpgcheck => 0,
+      enabled  => 1,
+      protect  => 0;
   }
 }
 
